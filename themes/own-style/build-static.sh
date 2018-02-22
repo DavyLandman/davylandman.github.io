d1b0fc2aa1eb96280ecf3ddff4ec04a5601f1e7b
Davy Landman <davy.landman@swat.engineering>
2018-02-22 08:56:50 +0100

    Just hand code it approach

--- themes/own-style/build-static.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-IFS=$'\n\t'
-
-LESS_INPUT="static/src/main.less"
-CSS_OUTPUT="static/css/main.css"
-
-lessc "$LESS_INPUT" "$CSS_OUTPUT"
-postcss --verbose --no-map --use autoprefixer -o "$CSS_OUTPUT" "$CSS_OUTPUT"
